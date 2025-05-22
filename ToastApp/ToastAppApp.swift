e82d2361a8be09686268708d5b8e100b44dc4028
Anton Marchanka <anthonymarchenko@gmail.com>
2025-05-22 22:37:32 +0400

    Initial Commit

---
+++ ToastApp/ToastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastAppApp.swift
+//  ToastApp
+//
+//  Created by Anton Marchanka on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ToastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToastAppTests/ToastAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToastAppTests.swift
+//  ToastAppTests
+//
+//  Created by Anton Marchanka on 5/22/25.
+//
+
+import Testing
+@testable import ToastApp
+
+struct ToastAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
